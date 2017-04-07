d71dff2675fa5d3349db28302058d44332030d31
zero1c03 <zero1c03@hotmail.com>
2017-04-08 02:01:58 +0800

    Add files via upload
    Improve the thread efficiency.
    Add close thread button.

--- getcam4.py
+++ getcam4.py
@@ -1,7 +1,10 @@
 import urllib
 import os
 import threading
+import subprocess
 from Tkinter import *
+
+# TODO(blade1204) Add real time file size text
 
 class getCam4Video(Frame):
 
@@ -22,7 +25,12 @@ class getCam4Video(Frame):
         self.get = Button(self)
         self.get["text"] = "Get"
         self.get["command"] = self.startMethod
-        self.get.grid(row = 2,column = 0)
+        self.get.grid(row=2, column=0)
+
+        self.get = Button(self)
+        self.get["text"] = "Stop"
+        self.get["command"] = self.stopMethod
+        self.get.grid(row=2, column=1)
 
         self.displayText = Label(self)
         self.displayText["text"] = "something happened"
@@ -36,23 +44,30 @@ class getCam4Video(Frame):
             self.displayText["text"] = self.inputField.get()
             self.getCam4(self.inputField.get())
 
+    def stopMethod(self):
+        self.displayText["text"] = "closed"
+        stdout.close()
+        stderr.close()
+        p.terminate()
+
     def getCam4(self, url):
         # url = "http://zh.cam4.com/sensationnow"
         # url = input()
         content = urllib.urlopen(url).read()
 
-
         videoAppUrlStart = 12
         videoAppUrl = ""
         while content[content.find("videoAppUrl") + videoAppUrlStart] != '&':
-            videoAppUrl += content[content.find("videoAppUrl") + videoAppUrlStart]
+            videoAppUrl += content[content.find("videoAppUrl") +
+                                   videoAppUrlStart]
             videoAppUrlStart += 1
         print videoAppUrl
 
         videoPlayUrlStart = 13
         videoPlayUrl = ""
         while content[content.find("videoPlayUrl") + videoPlayUrlStart] != '&':
-            videoPlayUrl += content[content.find("videoPlayUrl") + videoPlayUrlStart]
+            videoPlayUrl += content[content.find(
+                "videoPlayUrl") + videoPlayUrlStart]
             videoPlayUrlStart += 1
         print videoPlayUrl
 
@@ -63,17 +78,24 @@ class getCam4Video(Frame):
             playerUrlStart += 1
         print playerUrl
 
-        command = "rtmpdump -r " + videoAppUrl + " -y " + videoPlayUrl + " -W " + playerUrl + " -V -o " + videoPlayUrl + ".flv"
+        command = "rtmpdump -r " + videoAppUrl + " -y " + videoPlayUrl + \
+            " -W " + playerUrl + " -V -o " + videoPlayUrl + ".flv"
         print command
 
-        threading.Thread(target=self.startcatch(command), args=(), name = 'thread-catch').start()
-
+        threading.Thread(target=self.startcatch(command),
+                         args=(), name='thread-catch').start()
 
     def startcatch(self, command):
-        self.displayText["text"] = os.system(command)
+        global p
+        global stdout
+        global stderr
+        stdout = open("stdout.txt", "wb")
+        stderr = open("stderr.txt", "wb")
+        p = subprocess.Popen(command, stdout=stdout, stderr=stderr)
+        self.displayText["text"] = p
+        # subprocess.Popen([command],stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
 if __name__ == '__main__':
     root = Tk()
     app = getCam4Video(master=root)
     app.mainloop()
-    
